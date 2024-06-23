c87fd29d9cf5fd429e3d897eaf14bdd44e510e37
Júlio Cezar Koronati <jckoronati@hotmail.com>
2024-06-23 17:45:20 -0300

    style: remove unused component

--- src/components/Button.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-    const props = defineProps({ name: { default: null }, url: { default: null }  });
-</script>
-
-<template>
-    <button data-url="props.">
-        {{ props.url }}
-    </button>
-</template>
